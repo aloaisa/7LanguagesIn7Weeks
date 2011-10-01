d64b934c74957087ce9328e93983d1fe23364d92
Alvaro Garcia Loaisa <loaisa@gmail.com>
2011-10-01 11:27:00 +0200

    Exercises Day 2

---
+++ Ruby/day2.rb
@@ -0,0 +1,42 @@
+##########################################
+##
+## Language Ruby.
+## - Day 2
+##      Seven Languages in seven weeks
+##
+###########################################
+
+#- Keep the following things in mind when using blocks as part of your interface:
+#
+#	If you create a collection class that you need to traverse, build on top of Enumerable rather than reinventing the wheel.
+#	If you have shared code that differs only in the middle, create a helper method that yields a block in between the pre/postprocessing code to avoid duplication of effort.
+#	If you use the &block syntax, you can capture the code block provided to a method inside a variable. You can then store this and use it later, which is very useful for creating dynamic callbacks.
+#	Using a combination of &block and instance_eval, you can execute blocks within the context of arbitrary objects, which opens up a lot of doors for highly customized interfaces.
+#	The return value of yield (and block.call) is the same as the return value of the provided block.
+
+
+## Array to Hash
+class Array
+  def to_h(keys)
+    Hash[*keys.zip(self).flatten]
+  end
+end
+
+
+keys = Array['first', 'second', 'third', 'more']
+values = Array['1º', 2, '3º']
+
+values.to_h(keys)
+
+
+## Hash to Array
+hash = values.to_h(keys)
+puts hash.class
+
+hash.to_a
+hash.to_a.class
+
+# Iterate through a hash and a array
+hash.each {|a, b| puts "a: #{a} b: #{b}" }
+hash.to_a.each {|a, b| puts "a: #{a} b: #{b}" }
+
