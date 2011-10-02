fcbdf594a453cdcc2acac57d2b90adce5aefc5e7
Alvaro Garcia Loaisa <loaisa@gmail.com>
2011-10-02 21:19:25 +0200

    Ruby finished exercises

--- Ruby/day3.rb
+++ Ruby/day3.rb
@@ -32,6 +32,19 @@
 #	This should print "lions".
 #
 ##################################################################################
+## Array to Hash
+class Array
+  def to_h(keys)
+    Hash[*keys.zip(self).flatten]
+  end
+end
+
+
+keys = Array['first', 'second', 'third', 'more']
+values = Array['1º', 2, '3º']
+
+values.to_h(keys)
+
 
 module ActsAsCsv
 
@@ -53,12 +66,25 @@ module ActsAsCsv
       			@csv_contents = []
       			filename = self.class.to_s.downcase + '.txt'
       			file = File.new(filename)
+
       			@headers = file.gets.chomp.split(', ')
 
       			file.each do |row|
         			@csv_contents << row.chomp.split(', ')
       			end
     		end
+
+		def each
+			@csv_contents.each do |row|
+
+				if !row[0].nil?
+					keys = @headers[0].split(',')
+					values = row[0].split(',')
+
+					yield CsvRow.new(values.to_h(keys))
+				end
+			end
+		end
 
     		attr_accessor :headers, :csv_contents
     		
@@ -89,15 +115,18 @@ end
 
 
 csvRow = CsvRow.new(Hash['one' => 1, 'two' => 2])
-puts csvRow.one
-puts csvRow.two
+puts "csvRow.one: " << csvRow.one.to_s
+puts "csvRow.two: " << csvRow.two.to_s
+puts
 
 csvRow = CsvRow.new({'lions' => 'roar', 'tigers' => 'rooooaaarrrr'})
-puts csvRow.lions
-puts csvRow.tigers
+puts "csvRow.lions: " << csvRow.lions
+puts "csvRow.tigers: " << csvRow.tigers
+puts
 
 rubyCsv = RubyCsv.new
-rubyCsv.each {|row| puts row.PROGRAMNAME}
+puts "PROGRAMNAME, ADVERTISERCATEGORY"
+rubyCsv.each {|row| puts row.PROGRAMNAME << ", " << row.ADVERTISERCATEGORY}
 
 
 
