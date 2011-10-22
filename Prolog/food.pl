e1a7c5db81b764bcae07f44610d309cb1c7b38cc
Alvaro Garcia Loaisa <loaisa@gmail.com>
2011-10-22 23:16:05 +0200

    Continuing...

---
+++ Prolog/food.pl
@@ -0,0 +1,32 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Language Prolog.
+%% - Day 1
+%%      Seven Languages in seven weeks
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+food_type(velveeta, cheese).
+
+food_type(ritz, cracker).
+
+food_type(spam, meat).
+
+food_type(sausage, meat).
+
+food_type(jolt, soda).
+
+food_type(twinkie, dessert).
+
+flavor(sweet, dessert).
+
+flavor(savory, meat).
+
+flavor(savory, cheese).
+
+flavor(sweet, soda).
+
+food_flavor(X, Y) :- food_type(X, Z), flavor(Y, Z).
+
+
+
