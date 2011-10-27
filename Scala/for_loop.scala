a0e575385153e774b79892d7aef4e06fa114f004
Alvaro Garcia Loaisa <loaisa@gmail.com>
2011-10-27 21:34:03 +0200

    Continue...

---
+++ Scala/for_loop.scala
@@ -0,0 +1,21 @@
+/*******************************************
+**
+** Language Scala.
+** - Day 1
+**      Seven Languages in seven weeks
+**
+******************************************/
+
+def forLoop {
+        println( "for loop using Java-style iteration" )
+        
+        for(i <- 0 until args.length) {
+                println(args(i))
+        }
+}
+
+forLoop
+
+// Execute=> scala for_loop.scala its all in the grind
+
+
