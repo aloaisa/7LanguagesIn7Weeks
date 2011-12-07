3f25bb3cbbd86cd85cbf3ad8f244157c8778616b
Alvaro García Loaisa <loaisa@gmail.com>
2011-12-07 21:29:43 +0100

    First exercise.

---
+++ Clojure/src/Clojure/day1_exercises.clj
@@ -0,0 +1,34 @@
+/*********************************
+* Language Clojure.
+* - Day 1
+*      Seven Languages in seven weeks
+*
+********************************/
+
+// Implement a function called (big st n) that returns true if a string st
+//	is longer than n characters.
+
+(defn
+iprintln (big "example" 0))
+	big
+	[st n]
+	(> (count st) n)
+)
+ 
+(println (big "example" 3))
+//true
+//nil
+
+(println (big "example" 7))
+//false
+//nil
+
+(println (big "example" 8))
+//false
+//nil
+
+(println (big "example" 0))
+//true
+//nil
+ 
+
