0b0fbf40a080a4fac27720f8fa7e693c339aceea
Alvaro García Loaisa <loaisa@gmail.com>
2011-12-12 21:51:34 +0100

    Finished Clojure.

--- Clojure/src/Clojure/day3_exercises.clj
+++ Clojure/src/Clojure/day3_exercises.clj
@@ -68,3 +68,92 @@
  
 (println @accounts)
 
+
+
+;;;
+;;; In this section, I’m going to outline a single problem called sleeping
+;;; barber. It was created by Edsger Dijkstra in 1965. It has these charac-
+;;; teristics:
+;;;	 • A barber shop takes customers.
+;;;	 • Customers arrive at random intervals, from ten to thirty millisec-
+;;; onds.
+;;;	 • The barber shop has three chairs in the waiting room.
+;;;	 • The barber shop has one barber and one barber chair.
+;;;	 • When the barber’s chair is empty, a customer sits in the chair,
+;;; wakes up the barber, and gets a haircut.
+;;;	 • If the chairs are occupied, all new customers will turn away.
+;;;	 • Haircuts take twenty milliseconds.
+;;;	 • After a customer receives a haircut, he gets up and leaves.
+;;; Write a multithreaded program to determine how many haircuts a bar-
+;;; ber can give in ten seconds.
+;;;
+
+(def clients (ref clojure.lang.PersistentQueue/EMPTY))
+
+(def counter (atom 0))
+
+(def running? (atom true))
+
+(def seats 5)
+
+(def seconds 10)
+
+
+
+(defn cut [client]
+	(println "Cutting" client)
+	(Thread/sleep 20)
+	(swap! counter inc))
+
+
+(defn nextClient []
+	(dosync
+		(let [client (peek @clients)]
+			(if-not (nil? client)
+			(alter clients pop))
+			client)
+	)
+)
+
+
+(defn barber []
+	(let [client (nextClient)]
+		(if-not (nil? client)
+			(cut client)
+			(Thread/sleep 50)))
+	(if @running? (recur))
+)
+
+
+(defn addClient [client]
+	(dosync
+		(if (< (count @clients) seats)
+		(do (println client "Sitting")
+			(alter clients conj client))
+		
+			(println client "Next turn")
+		)
+	)
+)
+
+
+(defn shop [count]
+	(Thread/sleep (+ 10 (rand-int 20)))
+	(if @running?
+		(do (addClient (str "client " count))
+			(recur (inc count))
+		)
+	)
+)
+
+
+(.start (Thread. barber))
+
+(.start (Thread. #(shop 0)))
+
+(Thread/sleep (* 1000 seconds))
+
+(swap! running? not)
+
+(println @counter "Cuts in" seconds "seconds")
+
